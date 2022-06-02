abdb40fe780586dbf776fe7074bac637d92fc03a
vrdisoft <vrdisoft@gmail.com>
2022-06-02 15:21:51 +0430

    add logout button and username to header component

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,14 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./style/header.sass";
+import { useToken } from "../../context/tokenContext";
 
-function Header({ username }: { username: string }) {
+function Header() {
+  const { username } = useToken();
+  const navigate = useNavigate();
+  const handleOnclick = () => {
+    navigate("/login", { replace: true });
+  };
+
   return (
     <Row>
       <div className="header-continer">
         <div className="baner">Arvan Challenge</div>
         <div className="username">{`Welcome ${username}`}</div>
-        <div className="logout-button">Logout</div>
+        <div className="logout-button" onClick={handleOnclick}>
+          Logout
+        </div>
       </div>
     </Row>
   );
--- src/context/tokenContext.tsx
+++ src/context/tokenContext.tsx
@@ -1,10 +1,17 @@
-import React, { createContext, useContext, useState, useCallback } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  useCallback,
+  useMemo,
+} from "react";
 
 const TokenContext = createContext({});
 type TokenType = {
   token: string;
+  username: string;
   isLoggedIn: boolean;
-  loginContext: (token: string) => void;
+  loginContext: (token: string, username: string) => void;
   logoutContext: () => void;
 };
 
@@ -12,6 +19,7 @@ const useToken = () => {
   const tokenContext = useContext(TokenContext) as TokenType;
   return {
     token: tokenContext.token,
+    username: tokenContext.username,
     isLoggedIn: tokenContext.isLoggedIn,
     loginContext: tokenContext.loginContext,
     logoutContext: tokenContext.logoutContext,
@@ -20,41 +28,53 @@ const useToken = () => {
 
 const retrieveStoredToken = () => {
   const storedToken = localStorage.getItem("token");
-
+  const storedUsername = localStorage.getItem("username");
   return {
     storedToken,
+    storedUsername,
   };
 };
 
 const TokenProvider = ({ ...rest }) => {
   const tokenData = retrieveStoredToken();
 
-  let initialToken;
+  let initialToken, initialUsername;
   if (tokenData) {
     initialToken = tokenData.storedToken;
+    initialUsername = tokenData.storedUsername;
   }
 
   const [token, setToken] = useState(initialToken);
+  const [username, setUsername] = useState(initialUsername);
 
   const userIsLoggedIn = !!token;
 
   const logoutHandler = useCallback(() => {
     setToken(null);
     localStorage.removeItem("token");
+    setUsername(null);
+    localStorage.removeItem("username");
   }, []);
 
-  const loginHandler = (currentToken: string) => {
-    setToken(currentToken);
-    localStorage.setItem("token", currentToken);
-  };
+  const loginHandler = useCallback(
+    (currentToken: string, currentUsername: string) => {
+      setToken(currentToken);
+      setUsername(currentUsername);
+      localStorage.setItem("token", currentToken);
+      localStorage.setItem("username", currentUsername);
+    },
+    []
+  );
 
-  const contextValue = {
-    token: token,
-    isLoggedIn: userIsLoggedIn,
-    loginContext: loginHandler,
-    logoutContext: logoutHandler,
-  };
-
+  const contextValue = useMemo(() => {
+    return {
+      token: token,
+      username: username,
+      isLoggedIn: userIsLoggedIn,
+      loginContext: loginHandler,
+      logoutContext: logoutHandler,
+    };
+  }, [token, username, userIsLoggedIn, loginHandler, logoutHandler]);
   return <TokenContext.Provider value={contextValue} {...rest} />;
 };
 
